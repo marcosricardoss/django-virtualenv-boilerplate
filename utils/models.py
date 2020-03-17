9a16d94d5f805598ec6e404cc31eeb4ba111416b
Marcos Ricardo <marcosricardoss@gmail.com>
2020-03-17 18:30:15 -0300

    feat: add the 'MetaTagsMixin' to the 'utils' app model

--- utils/models.py
+++ utils/models.py
@@ -2,6 +2,8 @@ from urllib.parse import urlparse, urlunparse
 
 from django.conf import settings
 from django.db import models
+from django.template import loader
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -13,8 +15,10 @@ class CreationModificationDateMixin(models.Model):
     class Meta:
         abstract = True
 
-    created = models.DateTimeField(_("creation date and time"), auto_now_add=True)
-    updated = models.DateTimeField(_("modification date and time"), auto_now=True)
+    created = models.DateTimeField(
+        _("creation date and time"), auto_now_add=True)
+    updated = models.DateTimeField(
+        _("modification date and time"), auto_now=True)
 
 
 class UrlMixin(models.Model):
@@ -52,3 +56,58 @@ class UrlMixin(models.Model):
 
     def get_absolute_url(self):
         return self.get_url_path()
+
+
+class MetaTagsMixin(models.Model):
+    """
+    Abstract base class for generating meta tags
+    """
+    class Meta:
+        abstract = True
+
+    meta_keywords = models.CharField(
+        _("Keywords"),
+        max_length=255,
+        blank=True,
+        help_text=_("Separate keywords with commas."))
+    meta_description = models.CharField(
+        _("Description"),
+        max_length=255,
+        blank=True)
+    meta_author = models.CharField(
+        _("Author"),
+        max_length=255,
+        blank=True)
+    meta_copyright = models.CharField(
+        _("Copyright"),
+        max_length=255,
+        blank=True)
+
+    def get_meta(self, name, content):
+        tag = ""
+        if name and content:
+            tag = loader.render_to_string("utils/meta.html", {
+                "name": name,
+                "content": content,
+            })
+        return mark_safe(tag)
+
+    def get_meta_keywords(self):
+        return self.get_meta("keywords", self.meta_keywords)
+
+    def get_meta_description(self):
+        return self.get_meta("description", self.meta_description)
+
+    def get_meta_author(self):
+        return self.get_meta("author", self.meta_author)
+
+    def get_meta_copyright(self):
+        return self.get_meta("copyright", self.meta_copyright)
+
+    def get_meta_tags(self):
+        return mark_safe("\n".join((
+            self.get_meta_keywords(),
+            self.get_meta_description(),
+            self.get_meta_author(),
+            self.get_meta_copyright(),
+        )))
