e99b61625737ce9231030ad7095415f608b23947
Marcos Ricardo <marcosricardoss@gmail.com>
2020-03-17 11:02:24 -0300

    feat: add the 'UrlMixin' to the 'utils' app model

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,40 @@
+from urllib.parse import urlparse, urlunparse
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UrlMixin(models.Model):
+    """
+    A replacement for get_absolute_url()
+    Models extending this mixin should have
+    either get_url or get_url_path implemented.
+    """
+    class Meta:
+        abstract = True
+
+    def get_url(self):
+        if hasattr(self.get_url_path, "dont_recurse"):
+            raise NotImplementedError
+        try:
+            path = self.get_url_path()
+        except NotImplementedError:
+            raise
+        website_host = getattr(settings,
+                               "SITE_HOST",
+                               "localhost:8000")
+        return f"http://{website_host}/{path}"
+    get_url.dont_recurse = True
+
+    def get_url_path(self):
+        if hasattr(self.get_url, "dont_recurse"):
+            raise NotImplementedError
+        try:
+            url = self.get_url()
+        except NotImplementedError:
+            raise
+        bits = urlparse(url)
+        return urlunparse(("", "") + bits[2:])
+    get_url_path.dont_recurse = True
+
+    def get_absolute_url(self):
+        return self.get_url_path()
